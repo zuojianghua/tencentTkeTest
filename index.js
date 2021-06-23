d6e6fda688e7404b49941a0cc2e7575817520a95
zuojianghua <28842136@qq.com>
2021-06-23 19:37:19 +0800

    demo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: zuojianghua
  * @Date: 2021-06-23 18:33:32
- * @LastEditTime: 2021-06-23 18:39:22
+ * @LastEditTime: 2021-06-23 19:37:05
  * @LastEditors: zuojianghua
  * @Description:
  * @FilePath: /tencentTkeTest/index.js
@@ -11,7 +11,7 @@ const app = express();
 const port = 3333;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World!2");
 });
 
 app.listen(port, () => {
